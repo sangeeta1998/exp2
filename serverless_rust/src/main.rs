4d348a885ba2cde8cab84110360c62c9a8ed0533
your_github_username <sangeeta.kakati19@gmail.com>
2024-06-18 11:27:06 +0200

    dockerfile

--- serverless_rust/src/main.rs
+++ serverless_rust/src/main.rs
@@ -8,7 +8,6 @@ fn main() {
 #[no_mangle]
 pub extern "C" fn handler() -> i32 {
     let start = Instant::now();
-    // Simulate some work
     let result = (0..1000).fold(0, |acc, x| acc + x);
     let duration = start.elapsed();
     println!("Time elapsed in expensive_function() is: {:?}", duration);
--- serverless_wasm/src/lib.rs
+++ serverless_wasm/src/lib.rs
@@ -3,7 +3,6 @@ use std::time::Instant;
 #[no_mangle]
 pub extern "C" fn handler() -> i32 {
     let start = Instant::now();
-    // Simulate some work
     let result = (0..1000).fold(0, |acc, x| acc + x);
     let duration = start.elapsed();
     println!("Time elapsed in handler() is: {:?}", duration);
